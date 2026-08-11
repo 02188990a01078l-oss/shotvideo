f69d65037fc7f9002259780dde1e0db634a88ffd
02188990a01078l-oss <02188990a01078l@gmail.com>
2026-08-11 23:20:36 +0900

    Update movie.py

--- AIshot/movie.py
+++ AIshot/movie.py
@@ -8,6 +8,7 @@ Created on Wed Aug  5 20:26:29 2026
 import streamlit as st
 import movie_module as mo
 
+st.set_page_config(page_title="숏캐스트 - AI 숏드라마 OTT", page_icon="🎥")
 st.title("🎥 숏캐스트 (ShortCast)\n")
 st.markdown("이곳은 AI가 창조한 새로운 서사를 만나는 AI 숏드라마 전용 OTT입니다.\n")
 Select = st.selectbox("모든 카테고리", ['모든 카테고리', '취업/직장', '첫사랑', '판타지', '가족', '학교', '슬픈 연애'], width=130)
